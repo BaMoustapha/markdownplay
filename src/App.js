c0fe6b038f3fd571d0491e4c7893a06576e28e0f
BAMoustapha <bam480836@gmail.com>
2024-01-08 18:54:29 +0000

    ba

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import jsPDF from 'jspdf';
 
@@ -12,16 +13,23 @@ const App = () => {
 
   const download = () => {
     const doc = new jsPDF();
-    doc.text(newTodo, 15, 15);
+    const textLines = newTodo.split('\n');
+    
+    textLines.forEach((line, index) => {
+      const splitText = doc.splitTextToSize(line, doc.internal.pageSize.width - 20);
+      doc.text(10, 10 + index * 10, splitText);
+    });
+  
     doc.save("text.pdf");
-  };
+  }
 
   return (
-    <main className='container-fluid bg-dark'>
+    
+    <main className='container-fluid bg-black'>
       <div className='container'>
         <div className='row d-flex'>
           <h3 className='text-center me-5 text-white my-4'>Markdown Editor</h3>
-          <div className='col-md-6'>
+          <div className='col-md-7'>
             <textarea
               type='text'
               className='form-control mb-3 bg-secondary text-white'
@@ -29,8 +37,8 @@ const App = () => {
               onChange={handleOnChange}
             />
           </div>
-          <div className='col-md-6'>
-            <textarea className='form-control' value={newTodo} readOnly></textarea>
+          <div className='col-md-4'>
+            <textarea className='form-control text2' value={newTodo} readOnly></textarea>
           </div>
         </div>
       </div>
